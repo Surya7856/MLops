7bc5219bf22ce909ba7dc33e86da09c362a653d2
Surya <surya286760@gmail.com>
2024-07-31 05:40:20 +0530

    folders updation

--- template.py
+++ template.py
@@ -15,7 +15,7 @@ list_of_files=[
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exception",
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
@@ -35,6 +35,6 @@ for filepath in list_of_files:
         os.makedirs(filedir,exist_ok=True)
         # logging.info("Creating directory: {filedir} for file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getssize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,'w') as f:
             pass
